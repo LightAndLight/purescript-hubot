08f65b6f39e779e9f8816df45179090a85cb1c52
Isaac Elliott <isaace71295@gmail.com>
2016-08-04 19:09:09 +1000

    Fixed effect rows

--- src/Hubot.purs
+++ src/Hubot.purs
@@ -41,6 +41,6 @@ respond :: forall e. Regex -> Robot -> Aff (hubot :: HUBOT | e) Response
 respond reg robot = makeAff $ \err cb -> respondInternal reg cb robot
 
 
-foreign import send :: forall e. String -> Response -> Eff (send :: HUBOT | e) Unit
+foreign import send :: forall e. String -> Response -> Eff (hubot :: HUBOT | e) Unit
 
-foreign import reply :: forall e. String -> Response -> Eff (reply :: HUBOT | e) Unit
+foreign import reply :: forall e. String -> Response -> Eff (hubot :: HUBOT | e) Unit
