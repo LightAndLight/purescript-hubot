310e59fb3c31f90a17c5c13e5cc8ac676e254398
Isaac Elliott <isaace71295@gmail.com>
2016-08-04 18:42:03 +1000

    Use Aff for hear and respond

--- src/Hubot.purs
+++ src/Hubot.purs
@@ -9,8 +9,9 @@ module Hubot (
     , send
 ) where
 
-import Prelude (Unit)
+import Prelude (($), (<<<), Unit)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Aff (Aff, makeAff)
 import Control.Monad.Reader.Trans (ReaderT(..))
 import Data.Maybe (Maybe(..))
 import Data.String.Regex (Regex)
@@ -22,21 +23,23 @@ foreign import data Response :: *
 
 foreign import data HUBOT :: !
 
+
 foreign import matchInternal :: (String -> Maybe String) -> Maybe String -> Response -> Int -> Maybe String
 
 match :: Response -> Int -> Maybe String
 match = matchInternal Just Nothing
 
-foreign import hearInternal :: forall e. Regex -> (Response -> Eff e Unit) -> Robot -> Eff (hubot :: HUBOT | e) Unit
 
-hear :: forall e. Regex -> (Response -> Eff e Unit) -> ReaderT Robot (Eff (hubot :: HUBOT | e)) Unit
-hear reg cb = ReaderT (hearInternal reg cb)
+foreign import hearInternal :: forall e. Regex -> (Response -> Eff e Unit) -> Robot -> Eff e Unit
+
+hear :: forall e. Regex -> ReaderT Robot (Aff (hubot :: HUBOT | e)) Response
+hear reg = ReaderT $ \robot -> makeAff (\err cb -> hearInternal reg cb robot)
 
 
-foreign import respondInternal :: forall e. Regex -> (Response -> Eff e Unit) -> Robot -> Eff (hubot :: HUBOT | e) Unit
+foreign import respondInternal :: forall e. Regex -> (Response -> Eff e Unit) -> Robot -> Eff e Unit
 
-respond :: forall e. Regex -> (Response -> Eff e Unit) -> ReaderT Robot (Eff (hubot :: HUBOT | e)) Unit
-respond reg cb = ReaderT (respondInternal reg cb)
+respond :: forall e. Regex -> ReaderT Robot (Aff (hubot :: HUBOT | e)) Response
+respond reg = ReaderT $ \robot -> makeAff (\err cb -> respondInternal reg cb robot)
 
 
 foreign import send :: forall e. String -> Response -> Eff (send :: HUBOT | e) Unit
